0cdb9bcfbb58b4ad82de50f4ed6b9300ab46da4f
hhi464331-sudo <hhi464331@gmail.com>
2026-03-27 11:07:47 +0600

    Implement basic bot functionality with web server

--- bot.py
+++ bot.py
@@ -1,1 +1,33 @@
+import os
+import asyncio
+from aiogram import Bot, Dispatcher, types
+from aiohttp import web
 
+TOKEN = os.getenv("BOT_TOKEN")
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start'])
+async def start(msg: types.Message):
+    await msg.reply("Bot is working ✅")
+
+# web server for render
+async def web_server():
+    async def handler(request):
+        return web.Response(text="Bot Alive")
+
+    app = web.Application()
+    app.router.add_get("/", handler)
+
+    runner = web.AppRunner(app)
+    await runner.setup()
+    site = web.TCPSite(runner, "0.0.0.0", 10000)
+    await site.start()
+
+async def main():
+    await web_server()
+    await dp.start_polling()
+
+if __name__ == "__main__":
+    asyncio.run(main())
