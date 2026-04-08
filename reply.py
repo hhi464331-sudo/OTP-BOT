cff36de8b96a74d716032a4bcec4e0a81e164579
hhi464331-sudo <hhi464331@gmail.com>
2026-04-08 13:33:53 +0600

    Delete reply.py

--- reply.py
+++
@@ -1,8 +0,0 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-
-def main_menu():
-    """ইউজারের মেইন মেনু (100% Real)"""
-    keyboard = ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
-    keyboard.add(KeyboardButton("📱 Get Number"), KeyboardButton("🌍 Available Country"))
-    keyboard.add(KeyboardButton("☎️ Support"))
-    return keyboard
